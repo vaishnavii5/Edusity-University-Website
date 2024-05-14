b32acfa7b96d266106691ff36bb9d4fd79616e99
veelocity <vaishnavit.006@gmail.com>
2024-05-15 02:07:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle='Our Program' title='What We Offer'/>
+        <Programs />
+        <About />
+      </div>
+    </div>
+  )
+}
+
+export default App
