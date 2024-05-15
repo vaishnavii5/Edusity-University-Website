10cbe100e331705e868411f9201aa9c43ec0f18a
veelocity <vaishnavit.006@gmail.com>
2024-05-15 12:36:38 +0530

    edited vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-resolve: {
-  alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
-}
